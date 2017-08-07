9fb495cdace21c93729196de4143442e090c2694
M Ali <alig2x@outlook.com>
2017-08-07 11:47:14 +0100

    1.5 Debugging Dryptor

--- app/src/main/java/uk/co/mali/androidrealmencryptiontest/views/activities/MainActivity.java
+++ app/src/main/java/uk/co/mali/androidrealmencryptiontest/views/activities/MainActivity.java
@@ -28,7 +28,7 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
-import java.security.interfaces.RSAPrivateKey;
+import java.security.PrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -249,34 +249,34 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             KeyStore.PrivateKeyEntry privateKeyEntry= (KeyStore.PrivateKeyEntry) keyStore.getEntry(alias,null);
-            RSAPrivateKey privateKey = (RSAPrivateKey) privateKeyEntry.getPrivateKey();
-            Cipher output = Cipher.getInstance("RSA/ECB/PKCS1Padding", "AndroidOpenSSL");
+            PrivateKey privateKey = privateKeyEntry.getPrivateKey();
+            Cipher output = Cipher.getInstance("RSA/ECB/PKCS1Padding");
             output.init(Cipher.DECRYPT_MODE, privateKey);
 
             String cipherText = mEncryptedText.getText().toString();
             CipherInputStream cipherInputStream = new CipherInputStream(new ByteArrayInputStream(Base64.decode(cipherText,Base64.DEFAULT)),output);
 
-            ArrayList<Byte> values = new ArrayList<>();
+        ArrayList<Byte> values = new ArrayList<>();
 
-            int nextByte;
+        int nextByte;
 
-            while((nextByte = cipherInputStream.read())!= -1){
-                values.add((byte) nextByte);
-            }
-
-            byte[] bytes = new byte[values.size()];
-
-            for(int i = 0; i<bytes.length;i++){
-                bytes[i] = values.get(i).byteValue();
-            }
-
-            String finalText = new String(bytes,0,bytes.length,"UTF-8");
-            mDecryptText.setText(finalText);
-
-        } catch (Exception e) {
-            Toast.makeText(this, "Exception " + e.getMessage() + " occured", Toast.LENGTH_LONG).show();
-            Log.e(TAG, Log.getStackTraceString(e));
+        while((nextByte = cipherInputStream.read())!= -1){
+            values.add((byte) nextByte);
         }
+
+        byte[] bytes = new byte[values.size()];
+
+        for(int i = 0; i<bytes.length;i++){
+            bytes[i] = values.get(i).byteValue();
+        }
+
+        String finalText = new String(bytes,0,bytes.length,"UTF-8");
+        mDecryptText.setText(finalText);
+
+    } catch (Exception e) {
+        Toast.makeText(this, "Exception " + e.getMessage() + " occured", Toast.LENGTH_LONG).show();
+        Log.e(TAG, Log.getStackTraceString(e));
+    }
     }
 
     public class KeyRecyclerAdapter extends ArrayAdapter<String> {
